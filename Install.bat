0a063dfadf3ccb1e797c45ad39e7eec78682d71e
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-07-21 21:30:49 +0200

    Add files via upload

--- Install.bat
+++ Install.bat
@@ -15,29 +15,30 @@ rem !!!!!!!!!!!!!!!!!!!!!!
 net session >nul 2>&1
 if %errorlevel% == 0 (
     echo L'exécution est en tant qu'administrateur.
-	rem Ajout du script au Path
-	REM Chemin complet du répertoire contenant le script
-	set "script_dir=%back%;"
+    rem Ajout du script au Path
+    REM Chemin complet du répertoire contenant le script
+    set "script_dir=%back%;"
 
-	REM Vérifier si le chemin du répertoire du script est déjà présent dans le PATH
-	echo %PATH% | find /i "%script_dir%;" >nul
+    REM Vérifier si le chemin du répertoire du script est déjà présent dans le PATH
+    echo %PATH% | find /i "%script_dir%;" >nul
 
-	REM Si le chemin n'est pas trouvé (erreurlevel = 1), ajouter le répertoire au PATH
-	if %errorlevel% == 1 (
-		echo Ajout du répertoire au PATH...
-		setx PATH "%PATH%;%script_dir%;" /M
-		echo Le répertoire a été ajouté au PATH.
-		echo Y | powershell -command "Set-ExecutionPolicy RemoteSigned"
-		
-	) else (
-		echo Le répertoire est déjà présent dans le PATH.
-	)
+    REM Si le chemin n'est pas trouvé (erreurlevel = 1), ajouter le répertoire au PATH
+    if %errorlevel% == 1 (
+        echo Ajout du répertoire au PATH...
+        setx PATH "%PATH%;%script_dir%;" /M
+		powershell.exe -Command "$env:PATH += \";%script_dir%\"; [System.Environment]::SetEnvironmentVariable(\"PATH\", $env:PATH, \"Machine\")"
+        echo Le répertoire a été ajouté au PATH.
+        powershell -command "Set-ExecutionPolicy RemoteSigned -Force"
+    ) else (
+        echo Le répertoire est déjà présent dans le PATH.
+    )
 
-	REM Mettre à jour le chemin d'accès actuel pour cette session
-	set PATH=%PATH%;%script_dir%
+    REM Mettre à jour le chemin d'accès actuel pour cette session
+    set "PATH=%PATH%;%script_dir%"
 ) else (
     echo L'exécution n'est pas en tant qu'administrateur.
 )
+
 
 
 rem Verifier que le répertoire .\Download existe
